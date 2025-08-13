3122253473574bb0188bf1da34d2ea2b445f96b4
Shaira Causil <causil845@gmail.com>
2025-08-13 08:37:25 -0500

    Add files via upload

---
+++ apiLoggin/src/main/java/org/example/authapi/AuthApiApp.java
@@ -0,0 +1,12 @@
+package org.example.authapi;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthApiApp {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApiApp.class, args);
+    }
+}
---
+++ apiLoggin/src/main/java/org/example/authapi/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package org.example.authapi.repository;
+
+import org.example.authapi.model.User;
+import org.springframework.data.r2dbc.repository.R2dbcRepository;
+import reactor.core.publisher.Mono;
+
+public interface UserRepository extends R2dbcRepository<User, Long> {
+    Mono<User> findByUsername(String username);
+    Mono<User> findByEmail(String email);
+    Mono<User> findByResetToken(String resetToken);
+}
